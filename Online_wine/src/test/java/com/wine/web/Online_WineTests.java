f8936b71e5ed8821c06a45fe5a1973ac891d458b
strangecodee <131364622+strangecodee@users.noreply.github.com>
2024-11-27 00:03:42 +0530

    updated commit

--- Online_wine/src/test/java/com/wine/web/Online_WineTests.java
+++ Online_wine/src/test/java/com/wine/web/Online_WineTests.java
@@ -1,10 +1,10 @@
-package com.business;
+package com.wine.web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BusinessProjectApplicationTests {
+class Online_WineTests {
 
 	@Test
 	void contextLoads() {
